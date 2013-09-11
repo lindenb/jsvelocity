b483f79eefa102703995aafca95ab04025574f6f
Pierre Lindenbaum <plindenbaum@yahoo.fr>
2013-09-11 20:32:46 +0200

    tools

--- README.md
+++ README.md
@@ -8,6 +8,12 @@ Original post was http://plindenbaum.blogspot.fr/2011/11/processing-json-data-wi
 
 Compile
 -------
+Edit or create a file 'build.properties' in the base directory and set the properties for ivy.
+It could be:
+```
+ivy.jar.dir=${user.home}/.ivy
+ivy.install.version=2.3.0
+```
 
 The compile process requires the javaCC code compiler.
 
@@ -28,6 +34,18 @@ Options
 * -e (key) (json-expr) add this json into the context.
 * -f (key) (json-file) add this json into the context.
 * -i (key) and read stdin-json to the context.
+* -C (key) (class.qualified.Name) add this Class into the context.
+* -c (key) (class.qualified.Name) add an instance of Class into the context.
+* -o (base directory) output base directory (default: current directory)
+
+Default objects
+---------------
+* out : the writer (default is stdout). This object contains the following methods. close(), flush(), open(String), openIfMissing(String), getFile()
+* tool : utility containing the following method: escapeC(String), escapeXml(String), escapeHttp(String)
+* now: java.sql.Timestamp at startup
+
+
+
 
 Example:
 --------
@@ -64,6 +82,16 @@ A Velocity template file:
 <h1>${indi['name']}</h1>
 Age:${indi.age}<br/>${indi.duck}
 #end
+<h3>String Constructors</h3>
+<ul>
+#foreach($S in ${STRING.constructors})
+<li>${S}</li>
+#end
+</ul>
+<p>${tool.escapeXml("<>&")}</p>
+${out.open("/tmp/file.txt")}
+Hello
+${out.close()}
 </body>
 </html>
 ```
@@ -71,7 +99,9 @@ Age:${indi.age}<br/>${indi.duck}
 apply the template:
 ```bash
 $ java -jar dist/jsvelocity.jar \
-	-f all test.json test.vm
+	-f all test.json \
+	-C STRING java.lang.String \
+	test.vm
 
 <html>
 <body>
@@ -81,7 +111,32 @@ Age:8<br/>true
 Age:9<br/>true
 <h1>Loulou</h1>
 Age:10<br/>true
+<h3>String Constructors</h3>
+<ul>
+<li>public java.lang.String(byte[])</li>
+<li>public java.lang.String(byte[],int,int)</li>
+<li>public java.lang.String(byte[],java.nio.charset.Charset)</li>
+<li>public java.lang.String(byte[],java.lang.String) throws java.io.UnsupportedEncodingException</li>
+<li>public java.lang.String(byte[],int,int,java.nio.charset.Charset)</li>
+<li>public java.lang.String(java.lang.StringBuilder)</li>
+<li>public java.lang.String(java.lang.StringBuffer)</li>
+<li>public java.lang.String(int[],int,int)</li>
+<li>public java.lang.String(char[],int,int)</li>
+<li>public java.lang.String(char[])</li>
+<li>public java.lang.String(java.lang.String)</li>
+<li>public java.lang.String()</li>
+<li>public java.lang.String(byte[],int,int,java.lang.String) throws java.io.UnsupportedEncodingException</li>
+<li>public java.lang.String(byte[],int)</li>
+<li>public java.lang.String(byte[],int,int,int)</li>
+</ul>
+<p>&lt;&gt;&amp;</p>
 </body>
 </html>
+
+
+$ more /tmp/file.txt 
+Hello
+
+
 ```
 
--- src/main/java/com/github/lindenb/jsvelocity/JSVelocity.java
+++ src/main/java/com/github/lindenb/jsvelocity/JSVelocity.java
@@ -16,11 +16,178 @@ public class JSVelocity
 {
 private static final Logger LOG=Logger.getLogger("JSVelocity");
 VelocityContext context=new VelocityContext();
+private File outDir=null;
+
+public static class Picture
+	{
+	public String getBase64()
+		{
+		return "";
+		}
+	
+	public int getWidth()
+		{
+		return 1;
+		}
+	public int getHeight()
+		{
+		return 1;
+		}
+	}
+
+public static class Tool
+	{
+	
+	
+	
+	public String escapeC(Object o)
+		{
+		if(o==null) return "";
+		String s=String.valueOf(o);
+		StringBuilder b=new StringBuilder(s.length());
+		for(int i=0;i< s.length();++i)
+			{
+			char c=s.charAt(i);
+			switch(c)
+				{
+				case '\n': b.append("\\n");break;
+				case '\r': b.append("\\r");break;
+				case '\t': b.append("\\t");break;
+				case '\\': b.append("\\\\");break;
+				case '\'': b.append("\\\'");break;
+				case '\"': b.append("\\\"");break;
+				default: b.append(c);break;
+				}
+			}
+		return b.toString();
+		}
+	
+	public String escapeXml(Object o)
+		{
+		if(o==null) return "";
+		String s=String.valueOf(o);
+		StringBuilder b=new StringBuilder(s.length());
+		for(int i=0;i< s.length();++i)
+			{
+			char c=s.charAt(i);
+			switch(c)
+				{
+				case '<': b.append("&lt;");break;
+				case '>': b.append("&gt;");break;
+				case '&': b.append("&amp;");break;
+				case '\'': b.append("&apos;");break;
+				case '\"': b.append("&quot;");break;
+				default: b.append(c);break;
+				}
+			}
+		return b.toString();
+		}
+	
+	public String escapeHttp(Object o) throws Exception
+		{
+		if(o==null) return "";
+		return java.net.URLEncoder.encode(o.toString(),"UTF-8");
+		}
+	
+	@Override
+	public String toString()
+		{
+		return "jsvelocity.tool";
+		}
+	}
+
+
+public class MultiWriter
+	extends Writer
+	{
+	private PrintWriter pw=null;
+	public File filename=null;
+	
+	public File getFile()
+		{
+		return this.filename;
+		}
+	
+	public void _open(String fName,boolean ignoreIfExists)throws IOException
+		{
+		close();
+		
+		this.filename=new File(JSVelocity.this.outDir,fName);
+		if(ignoreIfExists && this.filename.exists())
+				{
+				return;
+				}
+		LOG.info("opening "+this.filename);
+		if(this.filename.getParentFile()!=null)
+			{
+			this.filename.getParentFile().mkdirs();
+			}
+		this.pw=new PrintWriter(this.filename);
+		}
+	
+	
+	public void open(String fName)throws IOException
+		{
+		_open(fName,false);
+		}
+		
+	public void openIfMissing(String fName)throws IOException
+		{
+		_open(fName,true);
+		}
+	
+	@Override
+	public void close() throws IOException
+		{
+		flush();
+		if(pw!=null)
+			{
+			LOG.info("closing "+this.filename);
+			pw.close();
+			pw=null;	
+			}
+		filename=null;
+		}
+
+	@Override
+	public void flush() throws IOException
+		{
+		if(pw!=null)
+			{
+			pw.flush();
+			}
+		else if(this.filename==null)
+			{
+			System.out.flush();
+			}
+		}
+
+	@Override
+	public void write(char[] cbuf, int off, int len) throws IOException
+		{
+		if(pw!=null)
+			{
+			pw.write(cbuf, off, len);		
+			}
+		else if(this.filename==null)
+			{
+			System.out.print(new String(cbuf, off, len));
+			}
+		}
+	@Override
+	public String toString()
+		{
+		return filename==null?"null":filename.toString();
+		}
+	}
+
 
 private void usage()
 	{
 	System.out.println("JS Velocity. Pierre Lindenbaum PhD. 2013.");
 	System.out.println("Options:");
+	System.err.println(" -C (key) (class.qualified.Name) add this Class into the context.");
+	System.err.println(" -c (key) (class.qualified.Name) add an instance of Class into the context.");
 	System.err.println(" -s (key) (string) add this string into the context.");
 	System.err.println(" -e (key) (json-expr) add this json into the context.");
 	System.err.println(" -f (key) (json-file) add this json into the context.");
@@ -37,6 +204,7 @@ private void put(String key,Object o)
 	}
 private void run(String args[]) throws Exception
         {
+        
        String readstdin=null;
         int optind=0;
         while(optind< args.length)
@@ -46,9 +214,42 @@ private void run(String args[]) throws Exception
 			usage();
                         return;
                         }
+                else if(args[optind].equals("-o") && optind+1< args.length)
+			{
+			outDir=new File(args[++optind]);
+			}
+
 		else if(args[optind].equals("-i") && optind+1< args.length)
 			{
 			readstdin=args[++optind];
+			}
+		else if(args[optind].equals("-C") && optind+2< args.length)
+			{
+			String key=args[++optind];
+			String className=args[++optind];
+			try
+				{
+				put(key,Class.forName(className));
+				}
+			catch(Exception err)
+				{
+				System.err.println("Cannot load class "+className);
+				System.exit(-1);
+				}
+			}
+		else if(args[optind].equals("-c") && optind+2< args.length)
+			{
+			String key=args[++optind];
+			String className=args[++optind];
+			try
+				{
+				put(key,Class.forName(className).newInstance());
+				}
+			catch(Exception err)
+				{
+				System.err.println("Cannot create instance of class "+className);
+				System.exit(-1);
+				}
 			}
 		else if(args[optind].equals("-s") && optind+2< args.length)
 			{
@@ -98,6 +299,10 @@ private void run(String args[]) throws Exception
 		return;
 		}
 	
+	MultiWriter out=new MultiWriter();
+	put("out",out);
+	put("tool",new Tool());
+	put("now",new java.sql.Timestamp(System.currentTimeMillis()));
 	File file=new File(args[optind++]);
 	LOG.info("Reading VELOCITY template from file "+file);
 	VelocityEngine ve = new VelocityEngine();
@@ -106,10 +311,8 @@ private void run(String args[]) throws Exception
 	if(file.getParent()!=null) ve.setProperty("file.resource.loader.path",file.getParent());
 	ve.init();
 	Template template = ve.getTemplate(file.getName());
-	PrintWriter w=new PrintWriter( System.out);
-	template.merge( this.context, w);
-	w.flush();
-	w.close();
+	template.merge( this.context, out);
+	out.close();
         }
 
 
